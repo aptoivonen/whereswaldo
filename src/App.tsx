148fcb65ee170e937eec47466e885fce5fbd1b56
Aleksi Toivonen <aptoivonen@mail.com>
2023-03-05 18:38:45 +0200

    Export wrapped app and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
-function App() {
+export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
 
-function WrappedApp() {
+export function WrappedApp() {
   return (
     <HashRouter>
       <App />
     </HashRouter>
   );
 }
-
-export default WrappedApp;
