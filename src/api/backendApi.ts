1ae64db14168bd3fabf6aef94282e25ae86454d7
Aleksi Toivonen <aptoivonen@mail.com>
2023-03-22 18:21:43 +0200

    Create getAll

--- src/api/backendApi.ts
+++ src/api/backendApi.ts
@@ -1,6 +1,6 @@
-import { doc, getDoc } from 'firebase/firestore';
-import { transformDoc } from './helpers';
-import type { BasicDocument } from './types';
+import { doc, getDoc, getDocs } from 'firebase/firestore';
+import { transformDoc, constructQuery } from './helpers';
+import type { BasicDocument, QueryOptions } from './types';
 import db from '@/config/firebaseConfig';
 
 /**
@@ -27,8 +27,32 @@ function get(collectionPath: string): Promise<BasicDocument | Error> {
 }
 
 /**
+ * Gets documents from firestore and returns them inside a promise.
+ * Accepts a few optional queryOptions.
+ * @param collectionId id of the collection
+ * @param [queryOptions] query, order or limit data
+ * @param [queryOptions.limit] number of entries to fetch
+ * @param [queryOptions.orderBy] order of entries
+ * @param queryOptions.orderBy.property name of property to sort by
+ * @param [queryOptions.orderBy.direction] 'asc' or 'desc'
+ * @returns Array of documents
+ */
+function getAll(collectionId: string, queryOptions: QueryOptions = {}) {
+  if (!collectionId) {
+    return Promise.reject(new Error('CollectionId was empty'));
+  }
+  const docsQuery = constructQuery(collectionId, db, queryOptions);
+
+  return getDocs(docsQuery).then(
+    (snapshot) =>
+      snapshot.docs.map(transformDoc).filter(Boolean) as BasicDocument[]
+  );
+}
+
+/**
  * Wraps around firebase functions providing promise-land functions.
  */
 export default {
   get,
+  getAll,
 };
