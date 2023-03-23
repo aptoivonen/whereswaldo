dda881f12e5b445d776e1ac541eed41918eb7183
Aleksi Toivonen <aptoivonen@mail.com>
2023-03-23 16:46:24 +0200

    Clean test rule

--- src/api/backendApi.test.ts
+++ src/api/backendApi.test.ts
@@ -17,8 +17,7 @@ const readWriteMessagesRule = `rules_version = '2';
 service cloud.firestore {
   match /databases/{database}/documents {
     match /messages/{messageId} {
-      allow read: if true;
-      allow write: if true;
+      allow read, write: if true;
     }
   }
 }`;
