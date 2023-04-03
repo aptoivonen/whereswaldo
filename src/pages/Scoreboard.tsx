0358128b2c3812cd7947aa8a0db6234568a9198b
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-03 16:51:00 +0300

    Create basic scoreboard component

---
+++ src/pages/Scoreboard.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Scoreboard() {
+  return (
+    <>
+      <h1>Scoreboard</h1>
+      <Link to="/">Go Home</Link>
+    </>
+  );
+}
+
+export default Scoreboard;
