29a9e84050075232d0d3481288d185532f072fc7
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-03 17:19:14 +0300

    Add basic playerId handling in ScoreboardPage

--- src/pages/ScoreboardPage.tsx
+++ src/pages/ScoreboardPage.tsx
@@ -1,9 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 function ScoreboardPage() {
+  const [searchParams] = useSearchParams();
+  const playerId = searchParams.get('playerId');
+
   return (
     <>
       <h1>Scoreboard</h1>
+      {!!playerId && <p>Searched playerId: {playerId}</p>}
       <Link to="/">Go Home</Link>
     </>
   );
