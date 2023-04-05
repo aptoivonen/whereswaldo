4465a70dc839cbd631d29c7e3bdaad8b68b85eb3
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-05 17:21:50 +0300

    Configure tailwind theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      red: { light: '#f37176', DEFAULT: '#f00025' },
+      blue: { light: '#8fb4ea', DEFAULT: '#2689e0' },
+      light: '#fdfdfe',
+      dark: '#000',
+      white: '#fff',
+      black: '#000',
+    },
     extend: {},
   },
   plugins: [],
