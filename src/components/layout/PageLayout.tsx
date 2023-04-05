af42574b4249821ac4171959319a0f1ff1ae3085
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-05 11:19:16 +0300

    Add nav to page layout

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,10 +1,29 @@
 import { Outlet } from 'react-router-dom';
+import NavBar from '@/components/navigation/NavBar';
+import Nav from '@/components/navigation/Nav';
+
+const brand = { route: '/', content: "Where's Waldo" };
+
+const navLinks = [
+  { id: 'home', route: '/', content: 'Home' },
+  { id: 'scoreboard', route: '/scoreboard', content: 'Scoreboard' },
+];
 
 function PageLayout() {
   return (
-    <div>
+    <>
+      <NavBar>
+        <NavBar.Brand to={brand.route}>{brand.content}</NavBar.Brand>
+        <Nav>
+          {navLinks.map((navLink) => (
+            <Nav.Link key={navLink.id} to={navLink.route}>
+              {navLink.content}
+            </Nav.Link>
+          ))}
+        </Nav>
+      </NavBar>
       <Outlet />
-    </div>
+    </>
   );
 }
 
