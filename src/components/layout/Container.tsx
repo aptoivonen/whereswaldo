2f2c9f23e6a47d76ed535b55bbd0f69fd182d71a
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-05 15:14:06 +0300

    Create Container

---
+++ src/components/layout/Container.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+
+export type ContainerProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+function Container({ children, className = '' }: ContainerProps) {
+  return (
+    <div
+      className={clsx(
+        'mx-auto w-full px-4 sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Container;
