866b8174aab2011eba147a1a4cf89d6744e5e83f
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-05 18:46:44 +0300

    Create logo link in nav bar

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -2,8 +2,11 @@ import { Outlet } from 'react-router-dom';
 import Container from '@/components/layout/Container';
 import NavBar from '@/components/navigation/NavBar';
 import Nav from '@/components/navigation/Nav';
+import brandImage from '@/assets/images/waldohead.png';
 
-const brand = { route: '/', content: "Where's Waldo" };
+const brand = {
+  route: '/',
+};
 
 const navLinks = [
   { id: 'home', route: '/', content: 'Home' },
@@ -15,7 +18,15 @@ function PageLayout() {
     <>
       <NavBar>
         <Container className="flex">
-          <NavBar.Brand to={brand.route}>{brand.content}</NavBar.Brand>
+          <NavBar.Brand to={brand.route}>
+            <span className="flex items-center">
+              <img className="h-24" src={brandImage} alt="Where's Waldo logo" />
+              <span className="text-5xl font-bold">
+                <p className="text-blue">Where&apos;s</p>
+                <p className="text-red">Waldo?</p>
+              </span>
+            </span>
+          </NavBar.Brand>
           <Nav>
             {navLinks.map((navLink) => (
               <Nav.Link key={navLink.id} to={navLink.route}>
