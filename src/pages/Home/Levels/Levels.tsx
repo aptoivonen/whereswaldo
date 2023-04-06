dde83f6f100ea8381d94d611e3e8d568925f0dd3
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-06 16:17:47 +0300

    Improve levels grid

--- src/pages/Home/Levels/Levels.tsx
+++ src/pages/Home/Levels/Levels.tsx
@@ -8,13 +8,16 @@ function Levels() {
       {isError && 'Error loading levels'}
       {isLoading && 'Loading...'}
       {isSuccess && levels.length > 0 && (
-        <div className="flex flex-wrap gap-8">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-8">
           {levels.map((level) => (
-            <div
-              key={level.id}
-              className="flex w-96 flex-col border-8 border-blue"
-            >
-              <img src={level.thumbnailUrl} alt={`Level ${level.id}`} />
+            <div key={level.id} className="flex flex-col border-8 border-blue">
+              <div className="relative pb-[50%]">
+                <img
+                  className="absolute block h-full w-full object-cover"
+                  src={level.thumbnailUrl}
+                  alt={`Level ${level.id}`}
+                />
+              </div>
               <p className="bg-blue p-2 text-xl text-white">{level.title}</p>
             </div>
           ))}
