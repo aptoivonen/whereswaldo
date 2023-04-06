f9d9fb7c853a7a410ed7ea417bd970546fe0b639
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-06 16:57:25 +0300

    Write some description in home page

--- src/pages/Home/HomePage/HomePage.tsx
+++ src/pages/Home/HomePage/HomePage.tsx
@@ -3,9 +3,14 @@ import Levels from '../Levels/Levels';
 function HomePage() {
   return (
     <>
-      <section>
-        <h1>Find Waldo!</h1>
-        <p>Find Waldo as quickly as possible</p>
+      <section aria-labelledby="waldo-heading">
+        <h1 className="text-3xl font-bold" id="waldo-heading">
+          Find Waldo!
+        </h1>
+        <p>
+          Waldo has been lost in the crowd. Can you find him? Also, the Wizard
+          and Odlaw are hiding.
+        </p>
       </section>
       <section aria-label="Levels">
         <Levels />
