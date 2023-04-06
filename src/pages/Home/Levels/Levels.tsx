370d401e40510c41e6c54ee8fa3660aa943ba749
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-06 16:37:17 +0300

    Add check for empty level list

--- src/pages/Home/Levels/Levels.tsx
+++ src/pages/Home/Levels/Levels.tsx
@@ -8,12 +8,14 @@ function Levels() {
     <>
       {isError && 'Error loading levels'}
       {isLoading && 'Loading...'}
-      {isSuccess && levels.length > 0 && (
+      {isSuccess && levels.length > 0 ? (
         <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-8">
           {levels.map((level) => (
             <LevelCard key={level.id} level={level} />
           ))}
         </div>
+      ) : (
+        <span className="italic">No levels found.</span>
       )}
     </>
   );
