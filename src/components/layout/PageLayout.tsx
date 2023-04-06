edfb9a2e33f48c9b575cd59e453e28863aaf36fd
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-06 09:39:50 +0300

    Improve nav bar styles

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -17,23 +17,29 @@ function PageLayout() {
   return (
     <>
       <NavBar>
-        <Container className="flex">
-          <NavBar.Brand to={brand.route}>
-            <span className="flex items-center">
-              <img className="h-24" src={brandImage} alt="Where's Waldo logo" />
-              <span className="text-5xl font-bold">
-                <p className="text-blue">Where&apos;s</p>
-                <p className="text-red">Waldo?</p>
+        <Container>
+          <div className="flex items-center">
+            <NavBar.Brand to={brand.route}>
+              <span className="flex items-center">
+                <img
+                  className="h-24"
+                  src={brandImage}
+                  alt="Where's Waldo logo"
+                />
+                <span className="text-5xl font-bold">
+                  <p className="text-blue">Where&apos;s</p>
+                  <p className="text-red">Waldo?</p>
+                </span>
               </span>
-            </span>
-          </NavBar.Brand>
-          <Nav>
-            {navLinks.map((navLink) => (
-              <Nav.Link key={navLink.id} to={navLink.route}>
-                {navLink.content}
-              </Nav.Link>
-            ))}
-          </Nav>
+            </NavBar.Brand>
+            <Nav>
+              {navLinks.map((navLink) => (
+                <Nav.Link key={navLink.id} to={navLink.route}>
+                  {navLink.content}
+                </Nav.Link>
+              ))}
+            </Nav>
+          </div>
         </Container>
       </NavBar>
       <Container>
