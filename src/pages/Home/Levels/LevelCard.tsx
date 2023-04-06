839c6fcd8ccb22b64553d312f83302c1be0d87a1
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-06 16:33:37 +0300

    Add link to level page in level card

--- src/pages/Home/Levels/LevelCard.tsx
+++ src/pages/Home/Levels/LevelCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { LevelInfo } from '@/model/types';
 
 type LevelCardProps = {
@@ -6,16 +7,18 @@ type LevelCardProps = {
 
 function LevelCard({ level }: LevelCardProps) {
   return (
-    <div className="flex flex-col border-8 border-blue">
-      <div className="relative pb-[50%]">
-        <img
-          className="absolute block h-full w-full object-cover"
-          src={level.thumbnailUrl}
-          alt={`${level.title} Level`}
-        />
+    <Link to={`level/${level.id}`}>
+      <div className="flex flex-col border-8 border-blue">
+        <div className="relative pb-[50%]">
+          <img
+            className="absolute block h-full w-full object-cover"
+            src={level.thumbnailUrl}
+            alt={`${level.title} Level`}
+          />
+        </div>
+        <p className="bg-blue p-2 text-xl text-white">{level.title}</p>
       </div>
-      <p className="bg-blue p-2 text-xl text-white">{level.title}</p>
-    </div>
+    </Link>
   );
 }
 
