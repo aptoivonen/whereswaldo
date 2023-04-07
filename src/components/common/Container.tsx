ac5364865e1c7295bce02f928b0a3092229fad47
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-07 10:06:47 +0300

    Refactor Container to use tailwind-merge

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,4 +1,4 @@
-import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export type ContainerProps = {
   children: React.ReactNode;
@@ -8,7 +8,7 @@ export type ContainerProps = {
 function Container({ children, className = '' }: ContainerProps) {
   return (
     <div
-      className={clsx(
+      className={twMerge(
         'mx-auto w-full px-4 sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl',
         className
       )}
