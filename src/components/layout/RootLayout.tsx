6b40d1f1c894e404725dcd4de94eb981d179511d
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-13 17:53:23 +0300

    Set Toaster in layout

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,8 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { Toaster } from '@/components/common';
 
 function RootLayout() {
   return (
     <div className="flex h-full flex-col">
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 4000,
+        }}
+      />
       <Outlet />
     </div>
   );
