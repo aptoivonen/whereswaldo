b59645a2aff355a8d7ff9f1817f4bc09c28a94f0
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-13 11:07:29 +0300

    Throw hard errors to error boundary

--- src/components/state/QueryClientProvider.tsx
+++ src/components/state/QueryClientProvider.tsx
@@ -1,10 +1,21 @@
 import type { ReactNode } from 'react';
 import {
+  QueryCache,
   QueryClient,
   QueryClientProvider as TanstackQueryClientProvider,
 } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) =>
+      console.log(error instanceof Error ? error.message : error),
+  }),
+  defaultOptions: {
+    queries: {
+      useErrorBoundary: (error, query) => query.state.data === undefined,
+    },
+  },
+});
 
 type Props = {
   children: ReactNode;
