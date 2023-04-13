2c582a436245cab6aae0d794eda55a510b112f95
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-13 18:33:06 +0300

    Create type for scores

--- src/model/types.ts
+++ src/model/types.ts
@@ -4,3 +4,8 @@ export interface LevelInfo extends BasicDocument {
   thumbnailUrl: string;
   title: string;
 }
+export interface Score extends BasicDocument {
+  playerId: string;
+  levelId: string;
+  time: number;
+}
