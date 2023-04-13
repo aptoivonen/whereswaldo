2b2706049eba0f1fbb8d7d702b3e057886a38e29
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-13 11:04:48 +0300

    Re-export common components in index

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,7 @@
 import 'react-loading-skeleton/dist/skeleton.css';
 
 export { default as Skeleton } from 'react-loading-skeleton';
+export { default as Alert } from './Alert';
+export { default as Card } from './Card';
+export { default as Container } from './Container';
+export { default as Grid } from './Grid';
