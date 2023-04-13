2b1619d1fdb126c1e9b5d47ceecdecbd3c129bf0
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-13 11:12:17 +0300

    Wrap ErrorBoundary

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,6 +2,7 @@
 import 'react-loading-skeleton/dist/skeleton.css';
 
 export { default as Skeleton } from 'react-loading-skeleton';
+export { ErrorBoundary } from 'react-error-boundary';
 export { default as Alert } from './Alert';
 export { default as Card } from './Card';
 export { default as Container } from './Container';
