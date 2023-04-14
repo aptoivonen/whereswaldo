dc09869c8e02a74c73e76289b0e4091ec62dc264
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-14 12:55:48 +0300

    Fix container width on mobile

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -9,7 +9,7 @@ function Container({ children, className = '' }: ContainerProps) {
   return (
     <div
       className={twMerge(
-        'mx-auto w-full px-4 sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl',
+        'mx-auto w-auto px-4 sm:w-full sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl',
         className
       )}
     >
