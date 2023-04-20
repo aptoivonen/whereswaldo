b5d4978ca58e6dc880c2ce09bff3417623506a37
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-20 21:07:53 +0300

    Use LevelViewer in level page

--- src/pages/Level/LevelPage/LevelPage.tsx
+++ src/pages/Level/LevelPage/LevelPage.tsx
@@ -1,12 +1,31 @@
 import { Link, useParams } from 'react-router-dom';
+import LevelViewer from '../LevelViewer/LevelViewer';
+import { ErrorBoundary } from '@/components/common';
 
 function LevelPage() {
   const { levelId } = useParams();
 
+  if (!levelId) {
+    return (
+      <p>
+        No level id provided. Select a level from the{' '}
+        <Link to="/">home page</Link>
+      </p>
+    );
+  }
+
   return (
     <>
-      <h1>Level: {levelId}</h1>
-      <Link to="/">Go Home</Link>
+      <h1 className="absolute top-4 left-4 z-20 bg-red px-2 py-1 text-white">
+        Level: {levelId}
+      </h1>
+      <ErrorBoundary
+        fallback={
+          <span className="italic">{`Error loading level ${levelId}`}</span>
+        }
+      >
+        <LevelViewer levelId={levelId} />
+      </ErrorBoundary>
     </>
   );
 }
