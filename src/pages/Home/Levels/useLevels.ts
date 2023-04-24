da96049e5d5332fc4db56ae85b90388a474bd846
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-24 08:47:53 +0300

    Fix useLevels with new field 'characters'

--- src/pages/Home/Levels/useLevels.ts
+++ src/pages/Home/Levels/useLevels.ts
@@ -7,21 +7,25 @@ const levelInfoList: LevelInfo[] = [
     id: '1',
     thumbnailUrl: 'https://placehold.co/600x400/orange/white/png?text=Level+1',
     title: 'Fair',
+    characters: ['Waldo', 'Wizard', 'Odlaw'],
   },
   {
     id: '2',
     thumbnailUrl: 'https://placehold.co/600x400/tomato/white/png?text=Level+2',
     title: 'At the Beach',
+    characters: ['Waldo', 'Wizard', 'Odlaw'],
   },
   {
     id: '3',
     thumbnailUrl: 'https://placehold.co/600x400/beige/black/png?text=Level+3',
     title: 'At the Beach',
+    characters: ['Waldo', 'Wizard', 'Odlaw'],
   },
   {
     id: '4',
     thumbnailUrl: 'https://placehold.co/600x400/grey/red/png?text=Level+4',
     title: 'At the Beach',
+    characters: ['Waldo', 'Wizard', 'Odlaw'],
   },
 ];
 
