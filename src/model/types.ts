09f709d7b5ec49aaac60fd447bf9a1403ec196d1
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-24 08:47:22 +0300

    Create type LevelGameInfo
    Also add 'characters' to LevelInfo.

--- src/model/types.ts
+++ src/model/types.ts
@@ -1,11 +1,20 @@
 import { BasicDocument } from '@/api/types';
 
+type Character = 'Waldo' | 'Wizard' | 'Odlaw';
+
 export interface LevelInfo extends BasicDocument {
   thumbnailUrl: string;
   title: string;
+  characters: Character[];
 }
 export interface Score extends BasicDocument {
   userName: string;
   levelId: string;
   time: number;
+}
+
+export interface LevelGameInfo extends BasicDocument {
+  imgUrl: string;
+  title: string;
+  characters: Character[];
 }
