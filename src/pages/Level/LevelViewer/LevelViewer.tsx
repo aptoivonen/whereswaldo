2c2c6468b44a797642425151883db0c1e67b5899
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-24 10:02:33 +0300

    Refactor level viewer to use hook

--- src/pages/Level/LevelViewer/LevelViewer.tsx
+++ src/pages/Level/LevelViewer/LevelViewer.tsx
@@ -1,34 +1,41 @@
 import { MouseEventHandler } from 'react';
 import { ZoomPanViewer } from '@/components/common';
+import getImageClickPosition from './getImageClickPosition';
+import useLevel from './useLevel';
 
 type LevelViewerProps = {
   levelId: string;
 };
 
 function LevelViewer({ levelId }: LevelViewerProps) {
+  const level = useLevel(levelId);
+
   const handleImageClick: MouseEventHandler<HTMLImageElement> = (e) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const [imageX, imageY, imageWidth, imageHeight] = [
-      Math.floor(e.clientX - rect.left),
-      Math.floor(e.clientY - rect.top),
-      rect.width,
-      rect.height,
-    ];
-    const [imagePercentageX, imagePercentageY] = [
-      Math.floor((100 * imageX) / imageWidth),
-      Math.floor((100 * imageY) / imageHeight),
-    ];
+    const [imagePercentageX, imagePercentageY] = getImageClickPosition(e);
     console.log({ imagePercentageX, imagePercentageY });
   };
 
+  if (!level.data) {
+    return (
+      <div className="flex h-full w-full items-center justify-center">
+        <p className="text-xl">Loading Level...</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="h-full w-full overflow-hidden">
-      <ZoomPanViewer
-        onImageClick={handleImageClick}
-        imgSrc="https://placehold.co/2000x1000"
-        imgAlt="A level"
-      />
-    </div>
+    <>
+      <h1 className="absolute top-4 left-4 z-20 bg-blue px-2 py-1 text-white">
+        Level: {level.data.title}
+      </h1>
+      <div className="h-full w-full overflow-hidden">
+        <ZoomPanViewer
+          onImageClick={handleImageClick}
+          imgSrc={level.data.imgUrl}
+          imgAlt={level.data.title}
+        />
+      </div>
+    </>
   );
 }
 
