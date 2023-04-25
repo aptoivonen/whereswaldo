fcd23f2997ba07dfae28a4841cf0ddb0eff7ec1d
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-25 09:25:06 +0300

    Create Toast component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -7,4 +7,5 @@ export { default as Alert } from './Alert';
 export { default as Card } from './Card';
 export { default as Container } from './Container';
 export { default as toast, Toaster } from 'react-hot-toast';
+export { default as Toast } from './Toast';
 export { default as ZoomPanViewer } from './ZoomPanViewer';
