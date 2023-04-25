b1e1801c02a700b24d18a6bf68befbb106751b94
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-25 12:22:11 +0300

    Refactor ZoomPanViewer to use children

--- src/components/common/ZoomPanViewer.tsx
+++ src/components/common/ZoomPanViewer.tsx
@@ -1,19 +1,11 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import { useState, MouseEventHandler } from 'react';
+import { useState } from 'react';
 import { PanViewer } from 'react-image-pan-zoom-rotate';
 
 export type ZoomPanViewerProps = {
-  imgSrc: string;
-  imgAlt: string;
-  onImageClick?: MouseEventHandler<HTMLImageElement>;
+  children: React.ReactNode;
 };
 
-function ZoomPanViewer({
-  imgSrc,
-  imgAlt,
-  onImageClick = () => {},
-}: ZoomPanViewerProps) {
+function ZoomPanViewer({ children }: ZoomPanViewerProps) {
   const [dx, setDx] = useState(0);
   const [dy, setDy] = useState(0);
   const [zoom, setZoom] = useState(1);
@@ -34,12 +26,7 @@ function ZoomPanViewer({
       rotation={0}
       key={`${dx}-${zoom}`}
     >
-      <img
-        onClick={onImageClick}
-        className="block w-full"
-        src={imgSrc}
-        alt={imgAlt}
-      />
+      {children}
     </PanViewer>
   );
 }
