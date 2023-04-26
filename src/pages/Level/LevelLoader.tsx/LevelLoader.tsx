ca330d845b4c73942ad75248e2d1a5e1591ea210
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-26 17:28:24 +0300

    Move data loading to LevelLoader

---
+++ src/pages/Level/LevelLoader.tsx/LevelLoader.tsx
@@ -0,0 +1,22 @@
+import LevelViewer from '../LevelViewer/LevelViewer';
+import useLevel from '../LevelViewer/useLevel';
+
+type LevelLoaderProps = {
+  levelId: string;
+};
+
+function LevelLoader({ levelId }: LevelLoaderProps) {
+  const level = useLevel(levelId);
+
+  if (!level.data) {
+    return (
+      <div className="flex h-full w-full items-center justify-center">
+        <p className="text-xl">Loading Level...</p>
+      </div>
+    );
+  }
+
+  return <LevelViewer level={level.data} />;
+}
+
+export default LevelLoader;
