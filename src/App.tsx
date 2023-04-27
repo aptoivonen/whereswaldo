4c3df6a9f284facda91398f7cb8de883156a10cc
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-27 10:19:54 +0300

    Use fallbackRender in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import QueryClientProvider from '@/components/state/QueryClientProvider';
-import { ErrorBoundary } from './components/common';
+import { ErrorBoundary, FallbackRender } from './components/common';
 import RootLayout from '@/components/layout/RootLayout';
 import PageLayout from '@/components/layout/PageLayout';
 import HomePage from '@/pages/Home/HomePage/HomePage';
@@ -27,9 +27,7 @@ export function WrappedApp() {
   return (
     <HashRouter>
       <QueryClientProvider>
-        <ErrorBoundary
-          fallback={<div className="text-2xl">Something went wrong.</div>}
-        >
+        <ErrorBoundary fallbackRender={FallbackRender}>
           <App />
         </ErrorBoundary>
       </QueryClientProvider>
