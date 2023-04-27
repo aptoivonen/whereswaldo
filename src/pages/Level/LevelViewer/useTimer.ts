9df6b924988565d4f1f7373a0e549419f47eba7b
Aleksi Toivonen <aptoivonen@mail.com>
2023-04-27 10:53:39 +0300

    Add stop to useTimer

--- src/pages/Level/LevelViewer/useTimer.ts
+++ src/pages/Level/LevelViewer/useTimer.ts
@@ -1,15 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function useTimer() {
   const [counter, setCounter] = useState(0);
+  const intervalRef = useRef<ReturnType<typeof setTimeout> | undefined>();
 
   useEffect(() => {
     const intervalId = setInterval(() => setCounter((c) => c + 1), 1000);
+    intervalRef.current = intervalId;
 
     return () => clearInterval(intervalId);
   }, []);
 
-  return counter;
+  const stop = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  return { counter, stop };
 }
 
 export default useTimer;
