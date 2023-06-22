0eef11996c9a5d687b5535bec108ef8074eac973
Aleksi Toivonen <aptoivonen@mail.com>
2023-06-22 16:12:35 +0300

    Make useScores to access firestore

--- src/pages/Scoreboard/Scoreboard/useScores.ts
+++ src/pages/Scoreboard/Scoreboard/useScores.ts
@@ -1,102 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import type { Score } from '@/model/types';
-import { AsyncReturnType } from '@/utils/types/types';
+import type { AsyncReturnType } from '@/utils/types/types';
+import backendApi from '@/api/backendApi';
 
-const scoresList: Score[] = [
-  {
-    id: '1111',
-    userName: 'Aleksi',
-    levelId: '1',
-    time: 62,
-  },
-  {
-    id: '2222',
-    userName: 'Ilkka',
-    levelId: '2',
-    time: 23,
-  },
-  {
-    id: '3333',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '4444',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '5555',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '6666',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '7777',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '8',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '9',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '10',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '11',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '12',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '13',
-    userName: 'Lauri',
-    levelId: '1',
-    time: 12,
-  },
-  {
-    id: '14',
-    userName: 'Eero',
-    levelId: '1',
-    time: 33,
-  },
-];
+function timeAscending(a: Score, b: Score) {
+  return a.time - b.time;
+}
 
-function getScoresSortedByTimeAscending(): Promise<Score[]> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, 1000);
-  }).then(() => scoresList.sort((a, b) => a.time - b.time));
+async function getScores(): Promise<Score[]> {
+  const rawScores = (await backendApi.getAll('scores')) as Score[];
+  return rawScores.sort(timeAscending);
 }
 
 function useScores() {
-  const queryFn = getScoresSortedByTimeAscending;
+  const queryFn = getScores;
   const queryKey = ['scores'];
 
   type TError = Error;
