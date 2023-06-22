2fbafc01ee6b016e47c8d382ebad5f5c07b3792d
Aleksi Toivonen <aptoivonen@mail.com>
2023-06-22 14:49:55 +0300

    Remove outdated test

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -2,20 +2,9 @@ import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
-import { WrappedApp, App } from './App';
+import { App } from './App';
 
 describe('App', () => {
-  it('Renders hello world', () => {
-    // ARRANGE
-    render(<WrappedApp />);
-    // ACT
-    // EXPECT
-    expect(
-      screen.getByRole('heading', {
-        level: 1,
-      })
-    ).toHaveTextContent(/hello world/i);
-  });
   it('Renders not found if invalid path', () => {
     render(
       <MemoryRouter initialEntries={['/non-existing-route']}>
