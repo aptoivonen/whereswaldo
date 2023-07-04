2a0ce882232a32544a38080f6ae1d751857e9669
Aleksi Toivonen <aptoivonen@mail.com>
2023-07-04 13:54:06 +0300

    Refactor model types with zod

--- src/model/types.ts
+++ src/model/types.ts
@@ -1,23 +1,59 @@
-import { BasicDocument } from '@/api/types';
+import { z } from 'zod';
+import mapImgUrl from '@/utils/helpers/mapImgUrl';
 
-export type Character = 'Waldo' | 'Wizard' | 'Odlaw';
+const BaseSchema = z.object({ id: z.string() });
+const BasicDocumentSchema = BaseSchema.passthrough();
+export type BasicDocument = z.infer<typeof BasicDocumentSchema>;
 
-export interface LevelInfo extends BasicDocument {
-  thumbnailUrl: string;
-  title: string;
-  characters: Character[];
+const CharacterSchema = z.enum(['Waldo', 'Wizard', 'Odlaw']);
+export type Character = z.infer<typeof CharacterSchema>;
+
+const LevelInfoSchema = z
+  .object({
+    thumbnailUrl: z.string().transform(mapImgUrl),
+    title: z.string(),
+    characters: z
+      .array(CharacterSchema)
+      .min(1, 'Levels must have at least one character to find')
+      .transform((arr) => new Set(arr)),
+  })
+  .merge(BaseSchema);
+export type LevelInfo = z.infer<typeof LevelInfoSchema>;
+export const LevelInfoArraySchema = z.array(LevelInfoSchema);
+
+export const LevelGameInfoSchema = z
+  .object({
+    imgUrl: z.string().transform(mapImgUrl),
+    title: z.string(),
+    characterCoordinates: z
+      .record(
+        CharacterSchema,
+        z.tuple([
+          z.number().int().gte(0).lte(100),
+          z.number().int().gte(0).lte(100),
+        ])
+      )
+      .refine((obj) => Object.values(obj).some((v) => v !== undefined), {
+        message: 'A level must have at least one character with coordinates',
+      }),
+    foundAcceptanceRadius: z.number().int().gte(0).lte(100),
+  })
+  .merge(BaseSchema);
+export type LevelGameInfo = z.infer<typeof LevelGameInfoSchema>;
+
+const ScoreSchema = z
+  .object({
+    userName: z.string(),
+    levelId: z.string(),
+    time: z.number(),
+  })
+  .merge(BaseSchema);
+export type Score = z.infer<typeof ScoreSchema>;
+
+function timeAscending(a: Score, b: Score) {
+  return a.time - b.time;
 }
-export interface Score extends BasicDocument {
-  userName: string;
-  levelId: string;
-  time: number;
-}
 
-export type CharacterCoordinates = { [K in Character]?: [number, number] };
-
-export interface LevelGameInfo extends BasicDocument {
-  imgUrl: string;
-  title: string;
-  characterCoordinates: CharacterCoordinates;
-  foundAcceptanceRadius: number;
-}
+export const ScoresSchema = z
+  .array(ScoreSchema)
+  .transform((arr) => arr.sort(timeAscending));
