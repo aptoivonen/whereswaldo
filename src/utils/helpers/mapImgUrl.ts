f1542c3ca59330e292b9ad8082afe3ed29c88431
Aleksi Toivonen <aptoivonen@mail.com>
2023-08-28 19:16:12 +0300

    Fix image path calculation for storage emulator

--- src/utils/helpers/mapImgUrl.ts
+++ src/utils/helpers/mapImgUrl.ts
@@ -1,4 +1,9 @@
 export default function getImgUrl(imgUrl: string): string {
-  const storageUrl = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+  const storageUrl: string = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+  // emulator
+  if (storageUrl.startsWith('127.0.0.1')) {
+    const projectId: string = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+    return `http://${storageUrl}/${projectId}.appspot.com/images/${imgUrl}`;
+  }
   return `https://firebasestorage.googleapis.com/v0/b/${storageUrl}/o/images%2F${imgUrl}?alt=media`;
 }
