2ec07395ae92a2cfe53a15d4ab4f3e61a8b6a64c
Aleksi Toivonen <aptoivonen@mail.com>
2023-09-19 12:05:23 +0300

    Reorganize setting up firebase for testing

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,10 +1,20 @@
 import { FirebaseApp, initializeApp } from 'firebase/app';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import {
+  getFirestore,
+  connectFirestoreEmulator,
+  type Firestore,
+} from 'firebase/firestore';
 import * as firebaseJson from '../../firebase.json';
 
-let app: FirebaseApp;
+const isProduction = import.meta.env.PROD;
+const isDevelopment = import.meta.env.DEV;
+const isTest = import.meta.env.MODE === 'test';
 
-if (import.meta.env.PROD) {
+const { port } = firebaseJson.emulators.firestore;
+let app: FirebaseApp;
+let db: Firestore;
+
+if (isProduction) {
   app = initializeApp({
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -13,15 +23,32 @@ if (import.meta.env.PROD) {
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
   });
-} else {
-  app = initializeApp({ projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID });
+  db = getFirestore(app);
 }
 
-const db = getFirestore(app);
-
-if (!import.meta.env.PROD) {
-  const { port } = firebaseJson.emulators.firestore;
+if (isDevelopment) {
+  app = initializeApp({
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  });
+  db = getFirestore(app);
   connectFirestoreEmulator(db, 'localhost', port);
 }
 
-export default db;
+// In testing, the firestore instance will be set by tests, just setting it here for types
+if (isTest) {
+  app = initializeApp({
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  });
+  db = getFirestore(app);
+}
+
+export function getDb(): Firestore {
+  return db;
+}
+
+// Used for testing
+export function setDb(newDb: Firestore): void {
+  db = newDb;
+}
