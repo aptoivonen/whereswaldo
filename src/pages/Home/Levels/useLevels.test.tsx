ad3e5afc8796f9a04adc4ae950d53acbbcc5e8fc
Aleksi Toivonen <aptoivonen@mail.com>
2023-09-19 15:44:28 +0300

    Update useLevels test file

--- src/pages/Home/Levels/useLevels.test.tsx
+++ src/pages/Home/Levels/useLevels.test.tsx
@@ -1,63 +1,22 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import fs from 'fs';
+import { describe, it } from 'vitest';
 import { renderHook, waitFor } from '@testing-library/react';
-import { describe, it, beforeEach, afterEach } from 'vitest';
-import { type Firestore } from 'firebase/firestore';
 import {
-  initializeTestEnvironment,
-  RulesTestEnvironment,
-} from '@firebase/rules-unit-testing';
-import { setDb } from '@/config/firebaseConfig';
-import * as firebaseJson from '../../../../firebase.json';
+  wrapper,
+  setupDescribe,
+  setupHookTest,
+  setDbWithoutRule,
+} from '@/tests/setupHookTests';
 import useLevels from './useLevels';
 import mapImgUrl from '@/utils/helpers/mapImgUrl';
 
-const MY_PROJECT_ID = 'demo-test-id-uselevel';
-const { port: FIRESTORE_EMULATOR_PORT } = firebaseJson.emulators.firestore;
-
-let testEnv: RulesTestEnvironment;
-let unauthedDb: Firestore;
-let queryClient: QueryClient;
-let wrapper: React.FC<{ children: React.ReactNode }>;
+setupHookTest('demo-test-id-uselevels');
 
 describe('UseLevels', () => {
-  beforeAll(async () => {
-    queryClient = new QueryClient({
-      defaultOptions: {
-        queries: {
-          retry: false,
-        },
-      },
-    });
-    wrapper = function Wrapper({ children }: { children: React.ReactNode }) {
-      return (
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      );
-    };
-  });
+  setupDescribe();
 
-  afterEach(async () => {
-    await testEnv.clearFirestore();
-  });
-
-  afterAll(async () => {
-    await testEnv.cleanup();
-  });
-
-  beforeEach(async () => {
-    testEnv = await initializeTestEnvironment({
-      projectId: MY_PROJECT_ID,
-      firestore: {
-        rules: fs.readFileSync('firestore.rules', 'utf8'),
-        host: '127.0.0.1',
-        port: FIRESTORE_EMULATOR_PORT,
-      },
-    });
-
-    // Setup initial levels data
-    await testEnv.withSecurityRulesDisabled((context) => {
+  it('gets correct data', async () => {
+    // Setup initial level data
+    await setDbWithoutRule((context) => {
       const firestoreWithoutRule = context.firestore();
       return firestoreWithoutRule
         .collection('levels')
@@ -71,13 +30,7 @@ describe('UseLevels', () => {
           foundAcceptanceRadius: 3,
         });
     });
-    unauthedDb = testEnv
-      .unauthenticatedContext()
-      .firestore() as unknown as Firestore;
-    setDb(unauthedDb);
-  });
 
-  it('gets correct data', async () => {
     const { result } = renderHook(() => useLevels(), { wrapper });
 
     await waitFor(() =>
