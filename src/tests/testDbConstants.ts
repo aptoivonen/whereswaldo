d90c2390ac50aa5d41a4d484a4027fc502a95fc9
Aleksi Toivonen <aptoivonen@mail.com>
2023-09-28 18:11:53 +0300

    Refactor test db constant

--- src/tests/testDbConstants.ts
+++ src/tests/testDbConstants.ts
@@ -20,24 +20,7 @@ const LEVELS_DB = {
 };
 
 const LEVELS_AND_SCORES_DB = {
-  levels: {
-    '1': {
-      imgUrl: 'level-1.jpg',
-      thumbnailUrl: 'thumbnail-level-1.jpg',
-      title: 'Level 1',
-      characterCoordinates: { Waldo: [1, 1] },
-      characters: ['Waldo'],
-      foundAcceptanceRadius: 3,
-    },
-    '2': {
-      imgUrl: 'level-2.jpg',
-      thumbnailUrl: 'thumbnail-level-2.jpg',
-      title: 'Level 2',
-      characterCoordinates: { Waldo: [1, 1], Odlaw: [22, 22] },
-      characters: ['Waldo', 'Odlaw'],
-      foundAcceptanceRadius: 5,
-    },
-  },
+  ...LEVELS_DB,
   scores: {
     abc: {
       userName: 'Mikko',
